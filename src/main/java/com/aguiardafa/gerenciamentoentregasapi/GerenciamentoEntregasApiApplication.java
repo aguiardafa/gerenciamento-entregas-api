1217fba7b25711029ad6e30ad7895ac8712c71ab
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-01-11 04:12:32 -0300

    feat(init): cria projeto spring boot

---
+++ src/main/java/com/aguiardafa/gerenciamentoentregasapi/GerenciamentoEntregasApiApplication.java
@@ -0,0 +1,13 @@
+package com.aguiardafa.gerenciamentoentregasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoEntregasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoEntregasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aguiardafa/gerenciamentoentregasapi/GerenciamentoEntregasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aguiardafa.gerenciamentoentregasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoEntregasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
