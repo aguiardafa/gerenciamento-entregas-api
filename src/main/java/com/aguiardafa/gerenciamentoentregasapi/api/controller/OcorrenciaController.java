9d179c9301147dd7820d7df1ede31e75ddb71ce1
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-01-27 17:02:28 -0300

    feat(Ocorrência): implementa o EndPoint listar no Controlador de Ocorrência

--- src/main/java/com/aguiardafa/gerenciamentoentregasapi/api/controller/OcorrenciaController.java
+++ src/main/java/com/aguiardafa/gerenciamentoentregasapi/api/controller/OcorrenciaController.java
@@ -3,13 +3,16 @@ package com.aguiardafa.gerenciamentoentregasapi.api.controller;
 import com.aguiardafa.gerenciamentoentregasapi.api.mapper.OcorrenciaMapper;
 import com.aguiardafa.gerenciamentoentregasapi.api.model.OcorrenciaRequest;
 import com.aguiardafa.gerenciamentoentregasapi.api.model.OcorrenciaResponse;
+import com.aguiardafa.gerenciamentoentregasapi.domain.model.Entrega;
 import com.aguiardafa.gerenciamentoentregasapi.domain.model.Ocorrencia;
+import com.aguiardafa.gerenciamentoentregasapi.domain.service.BuscaEntregaService;
 import com.aguiardafa.gerenciamentoentregasapi.domain.service.RegistroOcorrenciasService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -17,6 +20,7 @@ import javax.validation.Valid;
 public class OcorrenciaController {
 
     RegistroOcorrenciasService registroOcorrenciasService;
+    BuscaEntregaService buscaEntregaService;
     OcorrenciaMapper ocorrenciaMapper;
 
     @PostMapping
@@ -26,6 +30,12 @@ public class OcorrenciaController {
         Ocorrencia ocorrenciaRegistrada = registroOcorrenciasService
                 .registrar(entregaId, ocorrenciaRequest.getDescricao());
         return ocorrenciaMapper.toResponse(ocorrenciaRegistrada);
+    }
+
+    @GetMapping
+    public List<OcorrenciaResponse> listar(@PathVariable Long entregaId) {
+        Entrega entrega = buscaEntregaService.buscar(entregaId);
+        return ocorrenciaMapper.toCollectionResponse(entrega.getOcorrencias());
     }
 
 }
