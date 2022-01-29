f89ef04452c8255aa0482e85407790f57430b2cc
aguiardafa <diego.fernandes.aguiar@gmail.com>
2022-01-29 02:24:47 -0300

    feat(Home): implementa Endpoint index com mensagem de saudação

--- src/main/java/com/aguiardafa/gerenciamentoentregasapi/GerenciamentoEntregasApiApplication.java
+++ src/main/java/com/aguiardafa/gerenciamentoentregasapi/GerenciamentoEntregasApiApplication.java
@@ -2,12 +2,22 @@ package com.aguiardafa.gerenciamentoentregasapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class GerenciamentoEntregasApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GerenciamentoEntregasApiApplication.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String index() {
+        return "<h1>Bem vindo ao Sistema de Gerenciamento de Entregas (Projeto Back-end 'Rest API')</h1>";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentoEntregasApiApplication.class, args);
+    }
 
 }
